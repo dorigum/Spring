3bf1bcf6c0726a502de8c3d3e4ac5c43662a3d60
dorigum <41736468+dorigum@users.noreply.github.com>
2021-04-23 01:45:57 +0900

    Delete 세미프로젝트/src directory

--- 세미프로젝트/src/main/java/com/spring_project/moon/IMemberDAO.java
+++
@@ -1,8 +0,0 @@
-package com.spring_project.moon;
-
-import java.util.HashMap;
-
-public interface IMemberDAO {
-	MemberVO loginCheck(HashMap<String, Object> map);
-	MemberVO idCheck(String id);
-}
--- 세미프로젝트/src/main/java/com/spring_project/moon/IMemberService.java
+++
@@ -1,6 +0,0 @@
-package com.spring_project.moon;
-
-public interface IMemberService {
-	MemberVO loginCheck(String id, String pwd);
-	MemberVO idCheck(String id);
-}
