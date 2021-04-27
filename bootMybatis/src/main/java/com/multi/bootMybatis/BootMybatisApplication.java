0279aa932c6fa9ee50d1b420a8c99578dce98c84
dorigum <41736468+dorigum@users.noreply.github.com>
2021-04-27 18:00:51 +0900

    210427 Spring 파일 업로드/다운로드 실습

--- bootMybatis/src/main/java/com/multi/bootMybatis/BootMybatisApplication.java
+++ bootMybatis/src/main/java/com/multi/bootMybatis/BootMybatisApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.multi.file.FileDownloadController;
+import com.multi.file.FileUploadController;
 import com.multi.product.IProductDAO;
 import com.multi.product.ProductController;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses=ProductController.class)
+@ComponentScan(basePackageClasses=FileUploadController.class)
+@ComponentScan(basePackageClasses=FileDownloadController.class)
 @MapperScan(basePackageClasses=IProductDAO.class)
 public class BootMybatisApplication {
 
--- bootMybatis/src/main/webapp/WEB-INF/views/index.jsp
+++ bootMybatis/src/main/webapp/WEB-INF/views/index.jsp
@@ -9,6 +9,9 @@
 	<body>
 		index 페이지 입니다.<br><br>
 		<a href="product/productListView">전체 상품 조회</a><br><br>
-		<a href="product/productNewForm">상품 등록</a>
+		<a href="product/productNewForm">상품 등록</a><br><br><br><br>
+		
+		<a href="fileUploadForm">파일 업로드</a><br><br>
+		<a href="fileDownloadForm">파일 다운로드</a><br><br>
 	</body>
 </html>
